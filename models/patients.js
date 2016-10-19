0de708ae6ed29756ff205ff6962583a767e2c0f3
truongmm <truongmm@gmail.com>
2016-10-18 23:57:46 -0700

    Add Patient Creation POST Request

--- models/patients.js
+++ models/patients.js
@@ -10,6 +10,20 @@ function Patients() {
     });
   };
 
+  this.create = function(patient, res) {
+    connection.acquire(function(err, con) {
+      con.query('INSERT INTO patients SET ?', patient, function(err, results) {
+        con.release();
+        if (err) {
+          res.send({message: 'Patient creation failed', err: err});
+        } else {
+          patient.id = results.insertId;
+          res.send({message: 'Patient created successfully', patient: patient});
+        }
+      });
+    });
+  };
+
   this.getPatient = function(id, res) {
     connection.acquire(function(err, con) {
       con.query('SELECT * FROM patients WHERE id = ?', [id], function(err, results) {
--- routes/patientsRouter.js
+++ routes/patientsRouter.js
@@ -11,6 +11,10 @@ patientsRouter.route('/')
   Patients.get(res);
 })
 
+.post(function(req, res) {
+  Patients.create(req.body, res);
+})
+
 patientsRouter.route('/:patientId')
 .get(function (req, res, next) {
   Patients.getPatient(req.params.patientId, res);
